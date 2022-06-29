d86c44f7e51ff4d6fefcc67c2c60e4ee6c9929d9
Brendan McMillion <brendanmcmillion@gmail.com>
2022-06-29 16:58:54 -0700

    Write more tests for tree math.

--- tree/math_test.go
+++ tree/math_test.go
@@ -10,6 +10,18 @@ func assert(ok bool) {
 	}
 }
 
+func slicesEq(left, right []int) bool {
+	if len(left) != len(right) {
+		return false
+	}
+	for i := 0; i < len(left); i++ {
+		if left[i] != right[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func TestMath(t *testing.T) {
 	assert(log2(0) == 0)
 	assert(log2(8) == 3)
@@ -18,4 +30,19 @@ func TestMath(t *testing.T) {
 	assert(level(1) == 1)
 	assert(level(2) == 0)
 	assert(level(3) == 2)
+
+	assert(root(5) == 7)
+	assert(left(7) == 3)
+	assert(right(7, 8) == 11)
+
+	assert(parent(1, 4) == 3)
+	assert(parent(5, 4) == 3)
+
+	assert(sibling(13, 8) == 9)
+	assert(sibling(9, 8) == 13)
+
+	assert(slicesEq(directPath(4, 8), []int{5, 3, 7}))
+	assert(slicesEq(copath(4, 8), []int{6, 1, 11}))
+
+	assert(slicesEq(fullSubtrees(7, 6), []int{3, 9}))
 }
