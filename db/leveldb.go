3ef7d737ac8ccdfff54b01dda0f4f7caf4f54e3b
Brendan McMillion <brendanmcmillion@gmail.com>
2022-08-07 15:25:13 -0700

    Add LevelDB database implementation.

---
+++ db/leveldb.go
@@ -0,0 +1,159 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/errors"
+)
+
+func dup(in []byte) []byte {
+	out := make([]byte, len(in))
+	copy(out, in)
+	return out
+}
+
+// ldbConn is a wrapper around a base LevelDB database that handles batching
+// writes between commits transparently.
+type ldbConn struct {
+	conn  *leveldb.DB
+	batch map[string][]byte
+}
+
+func newLDBConn(conn *leveldb.DB) *ldbConn {
+	return &ldbConn{conn, make(map[string][]byte)}
+}
+
+func (c *ldbConn) Get(key string) ([]byte, error) {
+	if value, ok := c.batch[key]; ok {
+		return dup(value), nil
+	}
+	return c.conn.Get([]byte(key), nil)
+}
+
+func (c *ldbConn) Put(key string, value []byte) {
+	c.batch[key] = dup(value)
+}
+
+func (c *ldbConn) Commit() error {
+	var b *leveldb.Batch
+	for key, value := range c.batch {
+		if key == "root" {
+			continue
+		}
+		b.Put([]byte(key), value)
+	}
+	if err := c.conn.Write(b, nil); err != nil {
+		return err
+	}
+	if value, ok := c.batch["root"]; ok {
+		if err := c.conn.Put([]byte("root"), value, nil); err != nil {
+			return err
+		}
+	}
+
+	c.batch = make(map[string][]byte)
+	return nil
+}
+
+// ldbTransparencyStore implements the TransparencyStore interface over a
+// LevelDB database.
+type ldbTransparencyStore struct {
+	conn *ldbConn
+}
+
+func NewLDBTransparencyStore(file string) (TransparencyStore, error) {
+	conn, err := leveldb.OpenFile(file, nil)
+	if errors.IsCorrupted(err) {
+		conn, err = leveldb.RecoverFile(file, nil)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &ldbTransparencyStore{newLDBConn(conn)}, nil
+}
+
+func (ldb *ldbTransparencyStore) GetRoot() (*TransparencyTreeRoot, error) {
+	latest, err := ldb.conn.Get("root")
+	if err != nil {
+		return nil, err
+	}
+	out := &TransparencyTreeRoot{}
+	if err := json.Unmarshal(latest, out); err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (ldb *ldbTransparencyStore) SetRoot(root *TransparencyTreeRoot) error {
+	raw, err := json.Marshal(root)
+	if err != nil {
+		return err
+	}
+	ldb.conn.Put("root", raw)
+	return nil
+}
+
+func (ldb *ldbTransparencyStore) Get(key uint64) ([]byte, error) {
+	return ldb.conn.Get("t" + fmt.Sprint(key))
+}
+
+func (ldb *ldbTransparencyStore) Put(key uint64, data []byte) error {
+	ldb.conn.Put("t"+fmt.Sprint(key), data)
+	return nil
+}
+
+func (ldb *ldbTransparencyStore) LogStore() LogStore {
+	return &ldbLogStore{ldb.conn}
+}
+
+func (ldb *ldbTransparencyStore) PrefixStore() PrefixStore {
+	return &ldbPrefixStore{ldb.conn}
+}
+
+func (ldb *ldbTransparencyStore) Commit() error {
+	return ldb.conn.Commit()
+}
+
+// ldbLogStore implements the LogStore interface over LevelDB.
+type ldbLogStore struct {
+	conn *ldbConn
+}
+
+func (ls *ldbLogStore) BatchGet(keys []int) (map[int][]byte, error) {
+	out := make(map[int][]byte)
+
+	for _, key := range keys {
+		value, err := ls.conn.Get("l" + fmt.Sprint(key))
+		if err == leveldb.ErrNotFound {
+			continue
+		} else if err != nil {
+			return nil, err
+		}
+		out[key] = value
+	}
+
+	return out, nil
+}
+
+func (ls *ldbLogStore) BatchPut(data map[int][]byte) error {
+	for key, value := range data {
+		ls.conn.Put("l"+fmt.Sprint(key), value)
+	}
+	return nil
+}
+
+// ldbPrefixStore implements the PrefixStore interface over LevelDB.
+type ldbPrefixStore struct {
+	conn *ldbConn
+}
+
+func (ps *ldbPrefixStore) Get(key uint64) ([]byte, error) {
+	return ps.conn.Get("p" + fmt.Sprint(key))
+}
+
+func (ps *ldbPrefixStore) Put(key uint64, data []byte) error {
+	ps.conn.Put("p"+fmt.Sprint(key), data)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/JumpPrivacy/katie
 go 1.18
 
 require (
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
