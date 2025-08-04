1e1864011705181eb036b2f79d4520a72c21912a
Brendan McMillion <brendanmcmillion@gmail.com>
2025-08-03 19:29:57 -0700

    Update test workflow and generate-keys script

--- cmd/generate-keys/main.go
+++ cmd/generate-keys/main.go
@@ -1,35 +1,25 @@
-// Command generate-keys outputs fresh cryptographic keys for a Katie Server.
+// Command generate-keys outputs fresh cryptographic keys.
 package main
 
 import (
 	"crypto/ed25519"
 	"crypto/rand"
-	"crypto/x509"
-	"encoding/pem"
 	"fmt"
 	"log"
-
-	"github.com/Bren2010/katie/crypto/vrf/p256"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	fmt.Println()
 
-	signingKey := make([]byte, ed25519.SeedSize)
-	if _, err := rand.Read(signingKey); err != nil {
+	sigKey := make([]byte, ed25519.SeedSize)
+	if _, err := rand.Read(sigKey); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Signing Key:\n%x\n\n", signingKey)
+	fmt.Printf("Signing Private Key: %x\n", sigKey)
 
-	vrfPriv, _ := p256.GenerateKey()
-	raw, err := x509.MarshalECPrivateKey(vrfPriv.(*p256.PrivateKey).PrivateKey)
-	if err != nil {
+	vrfKey := make([]byte, ed25519.SeedSize)
+	if _, err := rand.Read(vrfKey); err != nil {
 		log.Fatal(err)
 	}
-	encoded := pem.EncodeToMemory(&pem.Block{
-		Type:  "VRF PRIVATE KEY",
-		Bytes: raw,
-	})
-	fmt.Printf("VRF Private Key:\n%s\n", encoded)
+	fmt.Printf("VRF Private Key:     %x\n", vrfKey)
 }
