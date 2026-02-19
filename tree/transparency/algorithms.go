9288d4494c1cca67f155aaac70ef7f5af8f2b50b
Brendan McMillion <brendanmcmillion@gmail.com>
2026-02-19 12:14:01 -0800

    Start implementing monitoring algorithms

--- tree/transparency/algorithms.go
+++ tree/transparency/algorithms.go
@@ -13,6 +13,9 @@ var (
 	ErrLabelExpired  = errors.New("requested version of label has expired")
 )
 
+func noLeftChild(x uint64) bool     { return math.IsLeaf(x) }
+func noRightChild(x, n uint64) bool { return math.IsLeaf(x) || x == n-1 }
+
 // rightmostDistinguished returns the position of the rightmost distinguished
 // log entry, or nil if there is none. The public config for the Transparency
 // Log is given in `config` and the size of the tree is `n`.
@@ -179,7 +182,7 @@ func fixedVersionSearch(config *structs.PublicConfig, ver uint32, n uint64, prov
 		// If the log entry is expired, recurse to its right child.
 		if config.IsExpired(timestamp, rightmost) {
 			expired = true
-			if math.IsLeaf(x) {
+			if noRightChild(x, n) {
 				return finish()
 			}
 			x, left = math.Right(x, n), timestamp
@@ -199,7 +202,7 @@ func fixedVersionSearch(config *structs.PublicConfig, ver uint32, n uint64, prov
 		// If the binary ladder indicates a greatest version less than the
 		// target version, then recurse to the right child.
 		if res == -1 {
-			if math.IsLeaf(x) || x == n-1 {
+			if noRightChild(x, n) {
 				return finish()
 			}
 			x, left = math.Right(x, n), timestamp
@@ -209,7 +212,7 @@ func fixedVersionSearch(config *structs.PublicConfig, ver uint32, n uint64, prov
 		// If the binary ladder indicates a greatest version greater than the
 		// target, then recurse to the left child.
 		if res == 1 {
-			if math.IsLeaf(x) {
+			if noLeftChild(x) {
 				return finish()
 			}
 			x, right = math.Left(x), timestamp
@@ -230,4 +233,143 @@ func fixedVersionSearch(config *structs.PublicConfig, ver uint32, n uint64, prov
 		} // Otherwise, return an error that the target version is expired.
 		return 0, ErrLabelExpired
 	}
+}
+
+type ownerState struct {
+	// starting is the position of the rightmost distinguished log entry that
+	// has been verified by owner monitoring.
+	starting uint64
+
+	// TODO
+}
+
+func (os *ownerState) greatestVersionAt(x uint64) uint32 {
+	panic("not implemented")
+}
+
+// monitoringReq contains the fixed portions of the state of a monitoring
+// request so that it is easier to pass into recursive functions.
+type monitoringReq struct {
+	config    *structs.PublicConfig
+	state     *ownerState
+	rightmost uint64
+	n         uint64
+
+	provider *dataProvider
+}
+
+// init returns the list of log entries to verify if an owner is starting their
+// monitoring at `req.state.starting`. `x` is the current log entry, and `left`
+// and `right` are the bounds used to determine distinguished status.
+//
+// Algorithm from the first portion of Section 8.3.
+func (req *monitoringReq) init(x, left, right uint64) ([]uint64, error) {
+	if !req.config.IsDistinguished(left, right) {
+		return nil, errors.New("requested starting position is not distinguished")
+	}
+	timestamp, err := req.provider.GetTimestamp(x)
+	if err != nil {
+		return nil, err
+	}
+	isExpired := req.config.IsExpired(timestamp, req.rightmost)
+
+	if x < req.state.starting {
+		// Starting position is to our right, so recurse right.
+		if noRightChild(x, req.n) {
+			return nil, errors.New("requested starting position is right of the rightmost log entry")
+		}
+		children, err := req.init(math.Right(x, req.n), timestamp, right)
+		if err != nil {
+			return nil, err
+		} else if isExpired {
+			return children, nil
+		}
+		return append(children, x), nil
+	} else if x == req.state.starting {
+		// We've reached the requested starting position. We already know it's
+		// distinguished and just need to verify that it's unexpired.
+		if isExpired {
+			return nil, errors.New("requested starting position is expired")
+		}
+		return []uint64{x}, nil
+	}
+	// Starting position is to our left, so recurse left.
+	if noLeftChild(x) || isExpired {
+		return nil, errors.New("requested starting position is invalid")
+	}
+	return req.init(math.Left(x), left, timestamp)
+}
+
+// monitor performs a depth-first walk of every unexpired distinguished log
+// entry that is to the right of `req.state.starting`, and requests a search
+// binary ladder from each of them for the expected greatest version of the
+// label.
+//
+// Algorithm from the second portion of Section 8.3.
+func (req *monitoringReq) monitor(x, left, right uint64) error {
+	// If the current log entry is not distinguished, stop.
+	if !req.config.IsDistinguished(left, right) {
+		return nil
+	}
+
+	// If the current log entry's index is less than or equal to that of the log
+	// entry advertised by the user, recurse to its right child.
+	if x <= req.state.starting {
+		if noRightChild(x, req.n) {
+			return nil
+		}
+		timestamp, err := req.provider.GetTimestamp(x)
+		if err != nil {
+			return err
+		}
+		return req.monitor(math.Right(x, req.n), timestamp, right)
+	}
+
+	// If the current log entry has a left child, recurse to the left child.
+	if !noLeftChild(x) {
+		timestamp, err := req.provider.GetTimestamp(x)
+		if err != nil {
+			return err
+		}
+		if err := req.monitor(math.Left(x), left, timestamp); err != nil {
+			return err
+		}
+	}
+
+	// If a stop condition has been reached, stop.
+	if req.stop() {
+		return nil
+	}
+
+	// Obtain a search binary ladder from the current log entry where the target
+	// version is the greatest version of the label expected to exist at this
+	// point, based on the label owner's state.
+	ver := req.state.greatestVersionAt(x)
+	res, err := req.provider.GetSearchBinaryLadder(x, ver, false)
+	if err != nil {
+		return err
+	} else if res != 0 {
+		return errors.New("binary ladder inconsistent with expected greatest version of label")
+	}
+
+	// If the current log entry has a right child, recurse to the right child.
+	if noRightChild(x, req.n) {
+		return nil
+	}
+	timestamp, err := req.provider.GetTimestamp(x)
+	if err != nil {
+		return err
+	}
+	return req.monitor(math.Right(x, req.n), timestamp, right)
+}
+
+func (req *monitoringReq) stop() bool {
+	// Stop conditions for user:
+	// - Out of search ladders.
+
+	// Stop conditions for server:
+	// - Output has reached maximum size.
+	// - Next binary ladder would be for version greater than user knows.
+
+	return false
 }
