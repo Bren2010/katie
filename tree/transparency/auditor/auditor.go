7b25012aaff106c747e6232f8b630229ecd79651
Brendan McMillion <brendanmcmillion@gmail.com>
2026-02-26 09:41:04 -0800

    Finish main auditor implementation

--- tree/transparency/auditor/auditor.go
+++ tree/transparency/auditor/auditor.go
@@ -1,3 +1,5 @@
+// Package auditor implements a stateful third-party auditor for a transparency
+// log.
 package auditor
 
 import (
@@ -6,12 +8,14 @@ import (
 
 	"github.com/Bren2010/katie/crypto/suites"
 	"github.com/Bren2010/katie/db"
+	"github.com/Bren2010/katie/tree/log"
 	"github.com/Bren2010/katie/tree/prefix"
 	"github.com/Bren2010/katie/tree/transparency/algorithms"
 	"github.com/Bren2010/katie/tree/transparency/math"
 	"github.com/Bren2010/katie/tree/transparency/structs"
 )
 
+// Auditor wraps the state of a third-party auditor.
 type Auditor struct {
 	config     *structs.PublicConfig
 	auditorKey suites.SigningPrivateKey
@@ -20,6 +24,9 @@ type Auditor struct {
 	state *AuditorState
 }
 
+// NewAuditor returns a new Auditor. `config` is the transparency log's public
+// configuration, `auditorKey` is the auditor's private key, and `tx` is a
+// connection to the auditor's storage.
 func NewAuditor(
 	config *structs.PublicConfig,
 	auditorKey suites.SigningPrivateKey,
@@ -55,28 +62,83 @@ func NewAuditor(
 	}, nil
 }
 
-func (a *Auditor) previousRightmost(added uint64) (*uint64, error) {
-	if a.state == nil {
-		return nil, nil
-	}
-	n := a.state.TreeHead.TreeSize
-
-	// Build the set of relevant log entry timestamps (the frontier timestamps
-	// we've retained and the new rightmost log entry timestamp), and pass them
-	// into a DataProvider as retained data.
+func (a *Auditor) previousRightmost(added uint64) (*uint64, *algorithms.DataProvider, error) {
+	// Build the set of relevant log entry timestamps (= the frontier timestamps
+	// we've retained + the new rightmost log entry timestamp).
+	n := uint64(0)
 	logEntries := make(map[uint64]structs.LogEntry)
-	for i, x := range math.Frontier(n) {
-		logEntries[x] = structs.LogEntry{Timestamp: a.state.Timestamps[i]}
+
+	if a.state != nil {
+		n = a.state.TreeHead.TreeSize
+
+		frontier := math.Frontier(n)
+		if len(frontier) != len(a.state.Timestamps) {
+			return nil, nil, errors.New("unexpected number of timestamps in auditor state")
+		}
+		for i, x := range frontier {
+			logEntries[x] = structs.LogEntry{Timestamp: a.state.Timestamps[i]}
+		}
 	}
+
 	logEntries[n] = structs.LogEntry{Timestamp: added}
 
+	// Pass the log entries into a DataProvider as retained state and compute
+	// the previous rightmost distinguished log entry.
 	provider := algorithms.NewDataProvider(a.config.Suite, nil)
 	provider.AddRetained(nil, logEntries)
+	prevDLE, err := algorithms.PreviousRightmost(a.config, n+1, provider)
+	if err != nil {
+		return nil, nil, err
+	}
 
-	// Compute the previous rightmost distinguished log entry.
-	return algorithms.PreviousRightmost(a.config, n+1, provider)
+	return prevDLE, provider, nil
 }
 
+func (a *Auditor) updateState(provider *algorithms.DataProvider, entry structs.LogEntry) error {
+	n := uint64(0)
+	var fullSubtrees [][]byte
+	if a.state != nil {
+		n = a.state.TreeHead.TreeSize
+		fullSubtrees = a.state.FullSubtrees
+	}
+
+	// Compute the new set of full subtrees of the log tree.
+	leaf, err := entry.Hash(a.config.Suite)
+	if err != nil {
+		return err
+	}
+	fullSubtrees, err = log.Append(a.config.Suite, n, fullSubtrees, leaf)
+	if err != nil {
+		return err
+	}
+
+	// Compute the new set of retained timestamps.
+	timestamps := make([]uint64, 0)
+	for _, x := range math.Frontier(n + 1) {
+		timestamp, err := provider.GetTimestamp(x)
+		if err != nil {
+			return err
+		}
+		timestamps = append(timestamps, timestamp)
+	}
+
+	a.state = &AuditorState{
+		TreeHead: structs.AuditorTreeHead{
+			Timestamp: entry.Timestamp,
+			TreeSize:  n + 1,
+			Signature: nil,
+		},
+		FullSubtrees: fullSubtrees,
+		Timestamps:   timestamps,
+		PrefixTree:   entry.PrefixTree,
+	}
+	return nil
+}
+
+// Process takes an AuditorUpdate as input and updates the auditor's internal
+// state, returning an error if any issues are detected. If the update fails to
+// process, no auditor state is changed. Successfully processed AuditorUpdate
+// structures are not persisted to the database until `Commit` is called.
 func (a *Auditor) Process(update *structs.AuditorUpdate) error {
 	// Verify that `timestamp` is greater than or equal to the rightmost log
 	// entry's timestamp.
@@ -118,7 +180,7 @@ func (a *Auditor) Process(update *structs.AuditorUpdate) error {
 
 	// For each element of `removed`, verify that the prefix leaf was published
 	// in at least one distinguished log entry.
-	prevDLE, err := a.previousRightmost(update.Timestamp)
+	prevDLE, provider, err := a.previousRightmost(update.Timestamp)
 	if err != nil {
 		return err
 	} else if prevDLE == nil && len(update.Removed) > 0 {
@@ -136,13 +198,50 @@ func (a *Auditor) Process(update *structs.AuditorUpdate) error {
 	before, after, err := prefix.EvaluateBeforeAfter(a.config.Suite, update.Added, update.Removed, &update.Proof)
 	if err != nil {
 		return err
-	} else if a.state != nil && !bytes.Equal(before, a.state.PrefixRoot) {
+	} else if a.state != nil && !bytes.Equal(before, a.state.PrefixTree) {
 		return errors.New("prefix tree root does not match expected")
 	}
 
-	// Compute the new root of the log tree.
+	// Update the auditor's state with the new log entry.
 	logEntry := structs.LogEntry{Timestamp: update.Timestamp, PrefixTree: after}
-
+	return a.updateState(provider, logEntry)
 }
 
-func (a *Auditor) TreeHead() *structs.AuditorTreeHead { return a.state.TreeHead }
+// Commit signs the auditor's tree head, commits it to the database, and returns
+// it.
+func (a *Auditor) Commit() (*structs.AuditorTreeHead, error) {
+	if a.state == nil {
+		return nil, errors.New("can not commit empty state")
+	} else if a.state.TreeHead.Signature != nil {
+		return &a.state.TreeHead, nil
+	}
+
+	// Sign the new auditor tree head.
+	root, err := log.Root(a.config.Suite, a.state.TreeHead.TreeSize, a.state.FullSubtrees)
+	if err != nil {
+		return nil, err
+	}
+	tbs, err := structs.Marshal(&structs.AuditorTreeHeadTBS{
+		Config:    a.config,
+		Timestamp: a.state.TreeHead.Timestamp,
+		TreeSize:  a.state.TreeHead.TreeSize,
+		Root:      root,
+	})
+	if err != nil {
+		return nil, err
+	}
+	a.state.TreeHead.Signature, err = a.auditorKey.Sign(tbs)
+	if err != nil {
+		return nil, err
+	}
+
+	// Serialize the auditor's state and commit it to the database.
+	raw, err := a.state.Marshal()
+	if err != nil {
+		return nil, err
+	} else if err := a.tx.PutState(raw); err != nil {
+		return nil, err
+	}
+
+	return &a.state.TreeHead, nil
+}
--- tree/transparency/auditor/state.go
+++ tree/transparency/auditor/state.go
@@ -1,12 +1,24 @@
 package auditor
 
-import "github.com/Bren2010/katie/tree/transparency/structs"
+import (
+	"bytes"
+
+	"github.com/Bren2010/katie/tree/transparency/structs"
+)
 
 type AuditorState struct {
-	TreeHead     *structs.AuditorTreeHead
+	TreeHead     structs.AuditorTreeHead
 	FullSubtrees [][]byte
 	Timestamps   []uint64
-	PrefixRoot   []byte
+	PrefixTree   []byte
+}
+
+func NewAuditorState(raw *bytes.Buffer) (*AuditorState, error) {
+	panic("not implemented")
+}
+
+func (as *AuditorState) Marshal() ([]byte, error) {
+	panic("not implemented")
 }
 
 func (as *AuditorState) AddedSince(x uint64, vrfOutput []byte) bool {
